da893c2d68d2d7ac72baaa78be4116b5ae405b3d
구승희 <pdw05460@gmail.com>
2021-01-04 21:44:28 +0900

    my_project

---
+++ my_project/우리동네 길고양이/assets/js/maps.js
@@ -0,0 +1,50 @@
+var mapContainer = document.getElementById('map'), // 지도를 표시할 div
+    mapOption = {
+        center: new kakao.maps.LatLng(37.54699, 127.09598), // 지도의 중심좌표
+        level: 3 // 지도의 확대 레벨
+    };
+var map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
+var imageSrc = "./static/cat_marker.png",
+    imageSize = new kakao.maps.Size(64, 69), // 마커이미지의 크기입니다
+    imageOption = {offset: new kakao.maps.Point(27, 69)}; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
+// 마커의 이미지정보를 가지고 있는 마커이미지를 생성합니다
+var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption),
+    markerPosition = new kakao.maps.LatLng(37.54699, 127.09598); // 마커가 표시될 위치입니다
+// 마커를 생성합니다
+var marker = new kakao.maps.Marker({
+    position: markerPosition,
+    image: markerImage // 마커이미지 설정
+});
+// 커스텀 오버레이에 표시할 컨텐츠 입니다
+// 커스텀 오버레이는 아래와 같이 사용자가 자유롭게 컨텐츠를 구성하고 이벤트를 제어할 수 있기 때문에
+// 별도의 이벤트 메소드를 제공하지 않습니다
+var content = '<div class="wrap">' +
+            '    <div class="info">' +
+            '        <div class="title">' +
+            '            쫑쫑이' +
+            '            <div class="close" onclick="closeOverlay()" title="닫기"></div>' +
+            '        </div>' +
+            '        <div class="body">' +
+            '            <div class="img">' +
+            '                <img src="https://images.mypetlife.co.kr/content/uploads/2019/09/06144600/zach-reiner-YVpr0OdSUe0-unsplash.jpg" width="73" height="70">' +
+            '           </div>' +
+            '            <div class="desc">' +
+            '                <div class="ellipsis">00시 00동 어쩌구 저쩌구</div>' +
+            '                <div class="comment">고목 나무 옆 쓰레기통에서 기웃거림.</div>' +
+            '            </div>' +
+            '        </div>' +
+            '    </div>' +
+            '</div>';
+// 마커 위에 커스텀오버레이를 표시합니다
+// 마커를 중심으로 커스텀 오버레이를 표시하기위해 CSS를 이용해 위치를 설정했습니다
+var overlay = new kakao.maps.CustomOverlay({
+    content: content,
+    map: map,
+    position: marker.getPosition()
+});
+// 마커를 클릭했을 때 커스텀 오버레이를 표시합니다
+marker.setMap(map);
+// 커스텀 오버레이를 닫기 위해 호출되는 함수입니다
+function closeOverlay() {
+    overlay.setMap(null);
+}
